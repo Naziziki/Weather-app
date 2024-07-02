67d0ea2c0cf887146fb5c4cc9856b4d61ebda689
Naziziki <nazarharin.nh@gmail.com>
2024-07-02 18:20:21 +0300

    added the ability to see more weather information, fixed bugs, updated the design

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+const showMore = document.querySelector('.showMore-container button');
+const showMoreDetailsContainer = document.querySelector('.showMore-container .showMore-weatherDetails');
 
-search.addEventListener('click', () => {
+
+search.addEventListener('click', function searchFunction() {
     const APIKey = '6ee27c8184bb80c0270db32179b59173';
     let city = document.querySelector('.search-box input').value;
 
@@ -20,12 +23,13 @@ search.addEventListener('click', () => {
             console.log(JSON.stringify(json, null, 2));
 
             if (json.cod === '404') {
-                container.style.height = '400px';
+                container.style.height = '370px';
                 error404.style.display = 'flex';
                 error404.classList.add('fadeIn');
                 weatherBox.style.display = 'none';
                 weatherDetails.style.display = 'none';
                 console.log("error404");
+                showMore.style.display = 'none';
                 return;
             }
 
@@ -37,6 +41,11 @@ search.addEventListener('click', () => {
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity p');
             const windSpeed = document.querySelector('.weather-details .wind-speed p');
+            // SHOW-MORE 
+            const feelsLikeTemp = document.querySelector('.weather-box .feelsLike-temp');
+            const windDeg = document.querySelector('.showMore-container .showMore-weatherDetails .wind-deg p');
+            const windGust = document.querySelector('.showMore-container .showMore-weatherDetails .wind-gust p');
+
 
             switch (json.weather[0].main) {
                 case 'Clouds':
@@ -67,14 +76,46 @@ search.addEventListener('click', () => {
                     mainImage.src = '';
             }
             temperature.innerHTML = `${parseInt(json.main.temp)}`;
-            description.innerHTML = `${json.weather[0].main}`;
+            description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             windSpeed.innerHTML = `${json.wind.speed}m/s`;
-
+            feelsLikeTemp.innerHTML = `Feels like ${parseInt(json.main.feels_like)}°C`;
+            windDeg.innerHTML = `${json.wind.deg}°`;
+            windGust.innerHTML = `${json.wind.gust}m/s`;
+            
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
+            container.style.height = '585px';
+
+            showMore.addEventListener('click', () => {
+                if (showMoreDetailsContainer.classList.contains('hide')) {
+                    showMoreDetailsContainer.classList.remove('hide');
+                    showMoreDetailsContainer.style.display = 'flex';
+                    showMore.style.padding = '7px 0 0 0';
+                    showMore.innerHTML = 'show less';
+                    container.style.height = '650px';
+                    feelsLikeTemp.style.display = 'block';
+                } else {
+                    showMoreDetailsContainer.classList.add('hide');
+                    showMore.style.padding = '0px';
+                    showMoreDetailsContainer.style.display = 'none';
+                    showMore.innerHTML = 'show more';
+                    container.style.height = '585px';
+                    feelsLikeTemp.style.display = 'none'
+                }
+            });
         });
 });
+
+// const input = document.getElementById('.search-box input');
+// input.addEventListener('keydown', function(event) {
+//   // If the user presses the "Enter" key on the keyboard
+//   if (event.key === 13) {
+//     // Cancel the default action, if needed
+//     // event.preventDefault();
+//     // Trigger the button element with a click
+//     search.click();
+//   }
+// });
